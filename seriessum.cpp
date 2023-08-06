c1a1a31d5c36b5d6eb699f58e62377021398d0d6
Anurag Bhattacharjee <105806479+AnuragB2004@users.noreply.github.com>
2023-08-06 10:00:05 +0530

    Add files via upload

---
+++ seriessum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "n: " << endl;
+    cin >> n;
+    int result = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            result -= i;
+        }
+        else
+        {
+            result += i;
+        }
+    }
+    cout << result << endl;
+    return 0;
+}
